d6a48319bba0f79104b6bb3f13088c0523627b6d
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-08-18 15:40:59 -0400

    api progress

--- lib/Flatiron_Project1_CLI_200818/earth_weather.rb
+++ lib/Flatiron_Project1_CLI_200818/earth_weather.rb
@@ -1,3 +1,19 @@
+require "pry"
+require "net/http"
+require "json"
+
 class EarthWeather
+
+    url = ""
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @@api_data = JSON.parse(response, symbolize_names: true)
+
+    binding.pry
+
+    def get_lat_longs
+
+    end
+
     
 end
--- lib/Flatiron_Project1_CLI_200818/martian_weather.rb
+++ lib/Flatiron_Project1_CLI_200818/martian_weather.rb
@@ -4,14 +4,26 @@ require "json"
 
 
 class MartianWeather
-
     attr_accessor :sol, :date, :season, :avgtemp, :hightemp, :lowtemp, :avgws, :highws, :lowws, :winddirection
+
     url = "https://api.nasa.gov/insight_weather/?api_key=dbgntr9dVwt1ol3Wdw5D8d7BTdEk5d208LElZEkA&feedtype=json&ver=1.0"
     uri = URI(url)
     response = Net::HTTP.get(uri)
-    martian_data = JSON.parse(response)    
-    # , symbolize_names: true
+    @@api_data = JSON.parse(response, symbolize_names: true)
 
-    # binding.pry
-    # puts "I'm Martian Weather class"
+    @@all = []
+
+    def initialize
+        @@api_data.each do |s| 
+            self.new
+            self.sol = s.first
+            save
+            binding.pry
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
 end
