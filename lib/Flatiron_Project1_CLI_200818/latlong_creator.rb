022448b712d8bdf6cfdaf16c738668b078c8fe1b
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-08-18 17:00:20 -0400

    Functional Mars api

---
+++ lib/Flatiron_Project1_CLI_200818/latlong_creator.rb
@@ -0,0 +1,3 @@
+class LatLongCreator
+
+end
--- lib/Flatiron_Project1_CLI_200818/martian_weather.rb
+++ lib/Flatiron_Project1_CLI_200818/martian_weather.rb
@@ -1,6 +1,7 @@
 require "pry"
 require "net/http"
 require "json"
+# require "./lib/environment"
 
 
 class MartianWeather
@@ -12,13 +13,28 @@ class MartianWeather
     @@api_data = JSON.parse(response, symbolize_names: true)
 
     @@all = []
+    # @@Martian_data = {}
 
+    
     def initialize
+    end
+
+    def self.create_instances
         @@api_data.each do |s| 
-            self.new
-            self.sol = s.first
-            save
+            o = self.new
+            o.sol = s[0].to_s
+            o.date = s[1][:Last_UTC].split("T").first
+            o.season = s[1][:Season]
+            o.avgtemp = s[1][:AT][:av]
+            o.hightemp = s[1][:AT][:mx]
+            o.lowtemp = s[1][:AT][:mn]
+            o.avgws = s[1][:HWS][:av]
+            o.highws = s[1][:HWS][:mx]
+            o.lowws = s[1][:HWS][:mn]
+            o.winddirection = s[1][:WD][:most_common][:compass_point]
+            o.save
             binding.pry
+            
         end
     end
 
@@ -26,4 +42,10 @@ class MartianWeather
         @@all << self
     end
 
+    def sort
+        @@all.sort_by[:sol]
+    end
+
 end
+
+MartianWeather.create_instances
