87e699d8df19e4bb6cf1686185ff08dafcb9d054
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-08-28 10:02:46 -0400

    uncommented welcomes

--- lib/Flatiron_Project1_CLI_200818/cli.rb
+++ lib/Flatiron_Project1_CLI_200818/cli.rb
@@ -9,13 +9,13 @@ class CLI
     end
 
     def start
-        # welcome
+        welcome
         get_valid_zip
         EarthWeather.create_instances(@lat, @long, @city, @state, @zip)
         MartianWeather.create_instances
         MartianWeather.create_forecast
         EarthWeather.create_forecast(@lat, @long, @city, @state, @zip)
-        # compare_current_weather_on_welcome
+        compare_current_weather_on_welcome
         main_menu
     end
 
